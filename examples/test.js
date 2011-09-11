b00a7cba1d9ed603c922ad92ae814677fbfe9efa
Josh Perez <josh@goatslacker.com>
2011-09-11 00:24:54 -0700

    added dotnotation preference over square bracket notation

--- examples/test.js
+++ examples/test.js
@@ -1,6 +1,20 @@
-var foo = function() {
+var foo = function () {
+  // missing semicolon
   dowhatnow()
   var test = null;
 
+  // missing semicolon
+  // already defined test
   var test = 12
+
+// missing semicolon
 }
+
+// missing space
+foo.bar = function() {
+  // missing space + semicolon
+  return 2+2 - 1*4
+};
+
+// can be expressed in dot notation
+foo["bar"]();
