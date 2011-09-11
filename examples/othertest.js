f8646b3907e52186d8cd699fb79478128a0e2f06
Josh Perez <josh@goatslacker.com>
2011-09-11 14:10:39 -0700

    more examples

---
+++ examples/othertest.js
@@ -0,0 +1,33 @@
+var a = 2.;
+
+var b = .0;
+
+var c = new Array();
+
+var d = new Object();
+
+function e ( )
+{
+  return
+  {
+    "foo": "bar"
+  };
+}
+
+if (a === NaN) {
+  // do nothing
+}
+
+function F(param) {
+  this.param = param;
+}
+
+var g = new F;
+
+var h = (function i() {
+  return "foo";
+})();
+
+var j = undefined;
+
+debugger;
