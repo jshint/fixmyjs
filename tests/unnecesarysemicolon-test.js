8f892720ccd414f169de9d746af9ae4d2d85cd72
Josh Perez <josh@goatslacker.com>
2011-09-16 17:13:07 -0700

    typo in test

--- tests/unnecesarysemicolon-test.js
+++ tests/unnecesarysemicolon-test.js
@@ -3,5 +3,5 @@ require('./')("Unnecessary semicolons", {
   "function foo() { };": "function foo() { }",
   "function foo() {\n};": "function foo() {\n}",
   "if (true) {\n};": "if (true) {\n}",
-  "function foo() { };function bar() { };": "function foo() { } function bar() { }"
+  "function foo() { }; function bar() { };": "function foo() { } function bar() { }"
 }).export(module);
