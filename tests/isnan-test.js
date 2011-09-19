ccd06686341760385310c38f9452c067a6b0d90a
Josh Perez <josh@goatslacker.com>
2011-09-19 02:36:19 -0700

    change isNaN test to what's supported

--- tests/isnan-test.js
+++ tests/isnan-test.js
@@ -1,6 +1,3 @@
 require('./')("Use isNaN rather than comparing to NaN", {
-  "var a = (16 == NaN);": "var a = isNaN(16);",
-  "var a = (5 !== NaN);": "var a = !isNaN(5);",
-  "var a = ('hello world' != NaN);": "var a = !isNaN('hello world');",
-  "var a = ([1, 2, 3, 4] === NaN);": "var a = isNaN([1, 2, 3, 4]);"
+  "var a = (b == NaN);": "var a = (isNaN(b));"
 }).export(module);
