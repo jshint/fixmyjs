e6ed243ea10daacf88e3fbde3a39c4ae53e1b8a1
Josh Perez <josh@goatslacker.com>
2012-01-29 05:15:00 -0800

    Adds koala for CI

--- test/vows-runner-test.js
+++
@@ -1,6 +0,0 @@
-var vows = require('vows');
-var tests = require('./specs');
-var files = (process.argv[1].indexOf('vows') === -1) ? process.argv[2] : undefined;
-tests.test.addFiles(files);
-files || tests.test.addTests();
-vows.describe('fixmyjs').addBatch(tests.test.specs).export(module);
