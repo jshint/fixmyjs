20211486e32c4e84ea30d1cfcb25397f4b578d1d
Josh Perez <josh@goatslacker.com>
2013-07-19 16:38:59 -0700

    Adds leading decimal test

---
+++ test/leading-decimal-test.js
@@ -0,0 +1,15 @@
+module.exports = function (f, l, assert) {
+  return {
+    'fix for leading decimals': function () {
+      var code = 'var a = .25;'
+      var result = 'var a = 0.25;'
+      assert.equal(f(code, {}), result)
+    },
+
+    'legacy does not fix leading decimals': function () {
+      var code = 'var a = .25;'
+      var result = 'var a = .25;'
+      assert.equal(l(code, {}), result)
+    }
+  }
+}
