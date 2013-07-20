8d6d26cc8a964d21a18e63f8a715bd59efaaa6ad
Josh Perez <josh@goatslacker.com>
2013-07-20 15:58:45 -0700

    Adds tests for camelCase conversion

---
+++ test/camelCase-test.js
@@ -0,0 +1,37 @@
+module.exports = function (f, assert) {
+  var options = { camelcase: true }
+  return {
+    'snake_case is converted to camelCase': function () {
+      var code = 'snake_case;'
+      var result = 'snakeCase;'
+      assert.equal(f(code, options), result)
+    },
+
+    'leading and trailing underscores are not converted': function () {
+      var code = '_private;'
+      assert.equal(f(code, options), code)
+
+      code = '__wtf;'
+      assert.equal(f(code, options), code)
+
+      code = 'yeah_;'
+      assert.equal(f(code, options), code)
+
+      code = 'triple___;'
+      assert.equal(f(code, options), code)
+
+      code = 'double__isGoodToo;'
+      assert.equal(f(code, options), code)
+    },
+
+    'properties are not converted': function () {
+      var code = 'var a = { snake_case: 1 };'
+      assert.equal(f(code, options), code)
+    },
+
+    'camelCase is not converted': function () {
+      var code = 'camelCase;'
+      assert.equal(f(code, options), code)
+    }
+  }
+}
