e8b98ee593bf77fce2b4375f763ea2f2a3b796c9
Josh Perez <josh@goatslacker.com>
2013-07-20 11:32:27 -0700

    Adds/Fixes test for leading-decimal/trailing-commas

--- test/leading-decimal-test.js
+++ test/leading-decimal-test.js
@@ -6,6 +6,12 @@ module.exports = function (f, l, assert) {
       assert.equal(f(code, {}), result)
     },
 
+    'leading decimals on member expressions': function () {
+      var code = 'Foo.bar = .25;'
+      var result = 'Foo.bar = 0.25;'
+      assert.equal(f(code, {}), result)
+    },
+
     'legacy does not fix leading decimals': function () {
       var code = 'var a = .25;'
       var result = 'var a = .25;'
--- test/trailing-commas-test.js
+++ test/trailing-commas-test.js
@@ -2,8 +2,8 @@ module.exports = function (f, assert) {
   return {
     'trailing commas are removed': function () {
       var code = 'foo([1, 2, 3,]);'
-      var result = 'foo([1,\n2,\n3]);'
-      assert.equal(f(code, {}), result)
+      var result = 'foo([\n  1,\n  2,\n  3\n]);'
+      assert.equal(f(code, { indent: 2 }), result)
     }
   }
 }
