b4eab0a5fcf3d703c1e048bb7775764852dffe90
Josh Perez <josh@goatslacker.com>
2013-07-20 14:27:16 -0700

    Adds a conditional way to add rules based on jshint options

--- test/curly-test.js
+++ test/curly-test.js
@@ -1,32 +1,33 @@
 module.exports = function (f, assert) {
+  var options = { curly: true }
   return {
     'for statements get curly braces': function () {
       var code = 'for (;;) x;'
       var result = 'for (;;) {\n  x;\n}'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, options), result)
     },
 
     'while statements with curly braces are left alone': function () {
       var code = 'while (true) {\n  x;\n}'
-      assert.equal(f(code, {}), code)
+      assert.equal(f(code, options), code)
     },
 
     'while statements get curlies': function () {
       var code = 'while (true) x;'
       var result = 'while (true) {\n  x;\n}'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, options), result)
     },
 
     'if statements get curly braces': function () {
       var code = 'if (x) x; else x;'
       var result = 'if (x) {\n  x;\n} else {\n  x;\n}'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, options), result)
     },
 
     'if statements with no alternate get curlies': function () {
       var code = 'if (x) x;'
       var result = 'if (x) {\n  x;\n}'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, options), result)
     }
   }
 }
--- test/update-expression-test.js
+++ test/update-expression-test.js
@@ -1,15 +1,17 @@
 module.exports = function (f, l, assert) {
+  var options = { plusplus: true }
+
   return {
     'plusplus is changed to += 1': function () {
       var code = 'a++;'
       var result = 'a += 1;'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, options), result)
     },
 
     'minusminus is changed to -= 1': function () {
       var code = 'a--;'
       var result = 'a -= 1;'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, options), result)
     }
   }
 }
