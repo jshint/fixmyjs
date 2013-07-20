d3c1485ee44d9ec95f0184a6e76e02b37834f939
Josh Perez <josh@goatslacker.com>
2013-07-20 00:48:56 -0700

    Fixes debugger test

--- test/debugger-test.js
+++ test/debugger-test.js
@@ -3,11 +3,6 @@ module.exports = function (f, l, assert) {
     'remove debugger statements': function () {
       var code = 'debugger;'
       assert.equal(f(code, {}), '')
-    },
-
-    'legacy removes debugger statements': function () {
-      var code = 'debugger;'
-      console.log(l(code, {}))
       assert.equal(l(code, {}), '')
     }
   }
