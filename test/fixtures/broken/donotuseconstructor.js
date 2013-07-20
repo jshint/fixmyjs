27c71539aad6e2dafcb207c1f5bbf81b31c33206
Josh Perez <josh@goatslacker.com>
2013-07-20 00:48:29 -0700

    Adds invalid construction tests

--- test/fixtures/broken/donotuseconstructor.js
+++
@@ -1,7 +0,0 @@
-new Number(16);
-var a = new JSON("foo");
-var b = new String("hello world");
-function c(e) {
-  return new Math(2 + 2);
-}
-var d = c(new Boolean(false));
--- test/fixtures/ok/donotuseconstructor.js
+++
@@ -1,7 +0,0 @@
-Number(16);
-var a = JSON("foo");
-var b = String("hello world");
-function c(e) {
-  return Math(2 + 2);
-}
-var d = c(Boolean(false));
