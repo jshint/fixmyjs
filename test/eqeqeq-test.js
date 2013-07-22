cd289c2730d4e5fcffe3fe5ec8d5f3e65ca05efd
Josh Perez <josh@goatslacker.com>
2013-07-22 01:45:22 -0700

    Adds experimental eqeqeq rule

---
+++ test/eqeqeq-test.js
@@ -0,0 +1,26 @@
+module.exports = function (f, assert) {
+  var options = { eqeqeq: true, _silent: true }
+  return {
+    '== is converted to ===': function () {
+      var code = 'a == null;'
+      var result = 'a === null;'
+      assert.equal(f(code, options), result)
+    },
+
+    '!= is converted to !==': function () {
+      var code = '1 != 2;'
+      var result = '1 !== 2;'
+      assert.equal(f(code, options), result)
+    },
+
+    '=== is not converted': function () {
+      var code = 'g === undefined;'
+      assert.equal(f(code, options), code)
+    },
+
+    '> is not converted': function () {
+      var code = '4 > 3;'
+      assert.equal(f(code, options), code)
+    }
+  }
+}
