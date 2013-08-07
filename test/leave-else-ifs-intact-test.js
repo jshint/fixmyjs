9a5630c6b732c857ca4c60dfd6d401b5ba5b2e93
Albert-Jan Nijburg <albertjan@curit.com>
2013-08-07 14:58:57 +0200

    leaves else if's in tact

--- test/leave-else-ifs-intact-test.js
+++ test/leave-else-ifs-intact-test.js
@@ -1,7 +1,7 @@
 module.exports = function (f, assert) {
     var options = { curly: true }
     return {
-        'if statements get curly braces': function () {
+        'leave elseif\'s be': function () {
             var code = 'if (x) x; else if (x || y) x;'
             var result = 'if (x) {\n  x;\n} else if (x || y) {\n  x;\n}'
             assert.equal(f(code, options), result)
