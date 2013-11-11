16ff3144e5d683cfdcace9519cb715ca707818da
Josh Perez <josh@goatslacker.com>
2013-11-11 10:51:26 -0800

    Fixes parseInt by placing it behind es3 option

--- test/parseInt-test.js
+++ test/parseInt-test.js
@@ -1,17 +1,18 @@
 module.exports = function (f, l, assert) {
+  var options = { es3: true }
   return {
     'parseInt gets the radix': function () {
       var code = 'parseInt(23820);'
       var result = 'parseInt(23820, 10);'
-      assert.equal(f(code, {}), result)
-      assert.equal(l(code, {}), result)
+      assert.equal(f(code, options), result)
+      assert.equal(l(code, options), result)
     },
 
     'parseInt works regardless of parentheses nesting': function () {
       var code = 'parseInt(someFunction(1));'
       var result = 'parseInt(someFunction(1), 10);'
-      assert.equal(f(code, {}), result)
-      assert.equal(l(code, {}), code)
+      assert.equal(f(code, options), result)
+      assert.equal(l(code, options), code)
     }
   }
 }
