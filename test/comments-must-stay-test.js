dcbb71b7956f33a81c3df5345d906f20b8911586
Josh Perez <josh@goatslacker.com>
2014-09-14 16:15:32 -0700

    Fix for leading/trailing decimals

--- test/comments-must-stay-test.js
+++ test/comments-must-stay-test.js
@@ -2,7 +2,7 @@ module.exports = function (f, l, assert) {
   return {
     'dont remove comments': function () {
       var code = 'var i = 0;\n//test\ni++;'
-      var result = 'var i = 0;\n\n//test\ni++;'
+      var result = 'var i = 0;\n//test\ni++;'
       var real = f(code, {});
       assert.equal(real, result)
     }
