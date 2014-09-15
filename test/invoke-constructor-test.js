7c3f1c4926be9998b8355601187c8ba4fd2104ad
Josh Perez <josh@goatslacker.com>
2014-09-14 17:46:40 -0700

    Fixes invocations and constructors test

--- test/invoke-constructor-test.js
+++ test/invoke-constructor-test.js
@@ -7,6 +7,20 @@ module.exports = function (f, l, assert) {
       assert.equal(l(code, {}), result)
     },
 
+    'constructors are invocations fixed': function () {
+      var code = 'var foo = new Foo();'
+      var result = 'var foo = new Foo();'
+      assert.equal(f(code, {}), result)
+      assert.equal(l(code, {}), result)
+    },
+
+    'constructors are invocations multi': function () {
+      var code = 'var foo = new Foo(1, 2, 3);'
+      var result = 'var foo = new Foo(1, 2, 3);'
+      assert.equal(f(code, {}), result)
+      assert.equal(l(code, {}), result)
+    },
+
     'constructors have invocation multiple statements': function () {
       var code = 'var foo = new Foo; var baz = new Baz(); var bar = new Bar;'
       assert.equal(l(code, {}), 'var foo = new Foo(); var baz = new Baz(); ' +
