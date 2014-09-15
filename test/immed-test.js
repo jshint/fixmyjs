80c5b4046bfb945354c122f265bc87b011db7822
Josh Perez <josh@goatslacker.com>
2014-09-14 17:47:16 -0700

    Reverts fixed test

--- test/immed-test.js
+++ test/immed-test.js
@@ -2,7 +2,7 @@ module.exports = function (f, l, assert) {
   return {
     'both accept dogballs': function () {
       var code = '(function () { })();'
-      var result = '(function() {})();'
+      var result = '(function () {})();'
       assert.equal(f(code, {}), result)
       assert.equal(l(code, {}), code)
     }
