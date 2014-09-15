cfad2d7251ba619e4261aeccd13abd1e3ed76f5b
Josh Perez <josh@goatslacker.com>
2014-09-14 18:19:47 -0700

    Recast wont do asi just yet

--- test/asi-test.js
+++ test/asi-test.js
@@ -1,9 +1,8 @@
-module.exports = function (f, l, assert) {
+module.exports = function (l, assert) {
   return {
     'semicolons are automatically inserted': function () {
       var code = 'var f'
       var result = 'var f;'
-      assert.equal(f(code, {}), result)
       assert.equal(l(code, {}), result)
     }
   }
