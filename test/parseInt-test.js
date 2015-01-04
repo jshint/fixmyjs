77a4c9f2b73ec8e2a027d7d985cf750276fbd30d
Josh Perez <josh@goatslacker.com>
2015-01-03 18:55:58 -0800

    Add a catch-all es3 option

--- test/parseInt-test.js
+++ test/parseInt-test.js
@@ -1,16 +1,15 @@
 module.exports = function (f, assert) {
-  var options = { es3: true }
   return {
     'parseInt gets the radix': function () {
       var code = 'parseInt(23820);'
       var result = 'parseInt(23820, 10);'
-      assert.equal(f(code, options), result)
+      assert.equal(f(code, { es3: false, parseIntRadix: true }), result)
     },
 
     'parseInt works regardless of parentheses nesting': function () {
       var code = 'parseInt(someFunction(1));'
       var result = 'parseInt(someFunction(1), 10);'
-      assert.equal(f(code, options), result)
+      assert.equal(f(code, { parseIntRadix: false, es3: true }), result)
     }
   }
 }
