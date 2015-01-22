8ad655edecc93a806558dc1334200bef848dff17
Josh Perez <josh@goatslacker.com>
2015-01-21 23:32:26 -0800

    Fixes decimals bug

--- test/leading-decimal-test.js
+++ test/leading-decimal-test.js
@@ -1,15 +1,21 @@
 module.exports = function (f, assert) {
+  var opts = { decimals: true }
   return {
     'fix for leading decimals': function () {
       var code = 'var a = .25;'
       var result = 'var a = 0.25;'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, opts), result)
     },
 
     'leading decimals on member expressions': function () {
       var code = 'Foo.bar = .25;'
       var result = 'Foo.bar = 0.25;'
-      assert.equal(f(code, {}), result)
+      assert.equal(f(code, opts), result)
+    },
+
+    'normal numbers': function () {
+      var code = 'var bar = 25;'
+      assert.equal(f(code, opts), code)
     }
   }
 }
